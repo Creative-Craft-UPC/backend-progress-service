70d318fff02279de0a787cbcf27524873266dbdf
Stevens Kharis Acha Esquerre <stevens.acha.13@gmail.com>
2025-09-26 23:46:09 -0500

    feat: update

--- schemas/record_schema.py
+++ schemas/record_schema.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
 
-from schemas.attempt_schema import AttemptSchema
+from schemas.attempt_schema import AttemptResponse
 
 class RecordSchema(BaseModel):
     max_time: float
@@ -14,7 +14,7 @@ class RecordResponse(BaseModel):
     id: str = Field(..., example="665f1b0c543ed91f9a1d0ef9")
     max_time: float
     min_time: float
-    attempts: List[AttemptSchema]
+    attempts: List[AttemptResponse]
     total_errors: int
     exercice_id: str = Field(..., example="665f1b0c543ed91f9a1d0ef9")
     
